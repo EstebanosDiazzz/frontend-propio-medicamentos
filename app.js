ca2682edb86bd864e679287dd2701ffcb2509f53
EstebanosDiazzz <diazjuanesteban3@gmail.com>
2025-04-06 03:39:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
           text: medicationDisplay
         }
       },
-      occurrenceDateTime: isoDate,   // Campo con typo que el backend valida
+      occurenceDateTime: isoDate,   // Campo con typo que el backend valida
       performer: [{
         actor: { reference: `Practitioner/${practitionerId}` }  // Asegúrate de que esto sea un string correcto
       }],
