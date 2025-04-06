36b4fd90a07621ded584da8272b4c70ac4c29257
EstebanosDiazzz <diazjuanesteban3@gmail.com>
2025-04-05 23:01:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ document.getElementById('medicationAdministrationForm').addEventListener('submit
     };
 
     // Enviar los datos usando Fetch API
-    fetch('https://backend-propio-6d0z.onrender.com', {
+    fetch('https://backend-propio-0z5h.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/fhir+json'
